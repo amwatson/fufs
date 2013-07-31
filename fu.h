b0632ffc2213b5f84f6a7946db9ae149ff5d7af0
Rick Benua <rick@benua.net>
2013-07-30 21:47:45 -0700

    added structures for indirect blocks

--- fu.h
+++ fu.h
@@ -18,4 +18,13 @@ struct fu_inode {
   uint32_t di_cluster;
   uint8_t si_index;
   uint8_t di_index;
+};
+
+struct fu_si_block {
+  uint32_t directs[32];
+};
+
+struct fu_di_block {
+  uint32_t clusters[25];
+  uint8_t indices[25];
 }
